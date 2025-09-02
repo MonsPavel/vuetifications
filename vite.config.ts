9ceff4ec94f63957fae80e492a2596210d9ee314
Mons Pavel <monspavel@gmail.com>
2025-09-02 23:48:25 +0300

    add storybook

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import { resolve, dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
-
 import vue from '@vitejs/plugin-vue';
+import path from 'node:path';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
-
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
+const __dirname = dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
   plugins: [vue()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'Vuetifications',
-      fileName: (format) => `vuetifications.${format}.js`
+      fileName: format => `vuetifications.${format}.js`
     },
     rollupOptions: {
       external: ['vue'],
@@ -23,5 +24,28 @@ export default defineConfig({
         }
       }
     }
+  },
+  test: {
+    projects: [{
+      extends: true,
+      plugins: [
+      // The plugin will run tests for the stories defined in your Storybook config
+      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+      storybookTest({
+        configDir: path.join(__dirname, '.storybook')
+      })],
+      test: {
+        name: 'storybook',
+        browser: {
+          enabled: true,
+          headless: true,
+          provider: 'playwright',
+          instances: [{
+            browser: 'chromium'
+          }]
+        },
+        setupFiles: ['.storybook/vitest.setup.ts']
+      }
+    }]
   }
 });
