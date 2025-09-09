10a8cd31ce08552b039e829f1d9469553c507668
MonsPavel <44977810+MonsPavel@users.noreply.github.com>
2025-09-09 21:50:38 +0300

    fix: clear timers

--- src/core/useNotifications.ts
+++ src/core/useNotifications.ts
@@ -1,9 +1,11 @@
 import { ref } from 'vue';
 
-import type { Notification } from '../types/notifications'
+import type { Notification  } from '../types/notifications'
 
 const notifications = ref<Notification[]>([]);
 let seed = 0;
+
+const timeoutMap = new Map<number, number | NodeJS.Timeout>();
 
 export function useNotifications() {
   const add = (options: Omit<Notification, 'id'>) => {
@@ -20,12 +22,27 @@ export function useNotifications() {
     notifications.value.push(n);
 
     if (n.duration && n.duration > 0) {
-      setTimeout(() => remove(id), n.duration);
+      const timeoutId = setTimeout(() => {
+        remove(id);
+      }, n.duration);
+      
+      timeoutMap.set(id, timeoutId);
     }
+
+    return id;
   };
 
   const remove = (id: number) => {
-    notifications.value = notifications.value.filter(n => n.id !== id);
+    if (timeoutMap.has(id)) {
+      clearTimeout(timeoutMap.get(id) as number);
+      timeoutMap.delete(id);
+    }
+
+    const index = notifications.value.findIndex(n => n.id === id);
+    
+    if (index > -1) {
+      notifications.value.splice(index, 1);
+    }
   };
 
   return { notifications, add, remove };
