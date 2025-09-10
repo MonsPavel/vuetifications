e18f39678f1918fa0a5439742796fbde8b618593
MonsPavel <44977810+MonsPavel@users.noreply.github.com>
2025-09-10 19:59:55 +0300

    feat: add shorthands methods

--- src/constants/notification.ts
+++ src/constants/notification.ts
@@ -1,4 +1,4 @@
-import type { NotificationPosition } from '../types/notifications';
+import type { NotificationPosition, Notification } from '../types/notifications';
 
 export const POSITIONS: NotificationPosition[] = [
   'top-left', 
@@ -6,3 +6,13 @@ export const POSITIONS: NotificationPosition[] = [
   'bottom-left', 
   'bottom-right'
 ];
+
+export const defaultOptions: Omit<Notification, 'id'> = {
+  type: 'info',
+  duration: 3000,
+  position: 'top-right',
+  closable: false,
+  animation: 'slide-fade',
+  title: '',
+  message: '',
+}
--- src/core/useNotifications.ts
+++ src/core/useNotifications.ts
@@ -2,6 +2,8 @@ import { ref } from 'vue';
 
 import type { Notification  } from '../types/notifications'
 
+import { defaultOptions } from '../constants/notification'
+
 const notifications = ref<Notification[]>([]);
 let seed = 0;
 
@@ -9,27 +11,23 @@ const timeoutMap = new Map<number, number | NodeJS.Timeout>();
 
 export function useNotifications() {
   const add = (options: Omit<Notification, 'id'>) => {
-    const id = ++seed;
     const n: Notification = {
-      id,
-      type: 'info',
-      duration: 3000,
-      position: 'top-right',
-      closable: false,
-      animation: options.animation || 'slide-fade',
+      id: ++seed,
+      ...defaultOptions,
       ...options
     };
+    
     notifications.value.push(n);
 
     if (n.duration && n.duration > 0) {
       const timeoutId = setTimeout(() => {
-        remove(id);
+        remove(n.id);
       }, n.duration);
       
-      timeoutMap.set(id, timeoutId);
+      timeoutMap.set(n.id, timeoutId);
     }
 
-    return id;
+    return n.id;
   };
 
   const remove = (id: number) => {
