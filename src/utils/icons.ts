f54d0970005302166059c533e777109a416bd574
MonsPavel <44977810+MonsPavel@users.noreply.github.com>
2026-06-20 11:12:24 +0300

    fix: small refactoring (#33)

--- src/utils/icons.ts
+++ src/utils/icons.ts
@@ -3,21 +3,21 @@ import errorIcon from '../assets/icons/error.svg'
 import infoIcon from '../assets/icons/info.svg'
 import warningIcon from '../assets/icons/warning.svg'
 
-import type { Notification } from '../types/notifications'
+import type { Notification, NotificationType } from '../types/notifications'
 
-const defaultIcons: Record<string, string> = {
+const defaultIcons: Partial<Record<NotificationType, string>> = {
   success: successIcon,
   error: errorIcon,
   info: infoIcon,
   warning: warningIcon,
 }
 
-export const getIcon = (n: Notification) => {
+export const getIcon = (n: Notification): string => {
   const { icon, type } = n
 
-  if(icon) return icon
+  if (icon) return icon
 
-  if(type) return defaultIcons[type]
+  if (type) return defaultIcons[type] ?? ''
 
   return ''
 }
